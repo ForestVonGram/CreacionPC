c1aef635e87fd89412a13caf591bcb434598d048
David <playa200612@gmail.com>
2024-10-02 14:17:30 -0500

    Se añade un logo y se actualiza el tamaño de la ventana al iniciar

--- src/main/java/org/uniquindio/ejercicioarchivos/MainApp.java
+++ src/main/java/org/uniquindio/ejercicioarchivos/MainApp.java
@@ -3,6 +3,7 @@ package org.uniquindio.ejercicioarchivos;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,13 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("PedidoView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Arma tu PC");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+
+        stage.setTitle("Arma tu PC con La Pecetera");
+
+        Image logo = new Image(getClass().getResourceAsStream("imagenes/logo-la-pecetera.png"));
+        stage.getIcons().add(logo);
+
         stage.setScene(scene);
         stage.show();
     }
